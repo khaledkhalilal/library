e2ed68d7b49d51e4873818fbdcfa40b6111332ca
khaled <aa19ss2019@gmail.com>
2025-12-03 22:43:29 +0300

    library

--- src/DeleteBook.java
+++ src/DeleteBook.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class DeleteBook {
     static Scanner in = new Scanner(System.in);
     static void runDeleteBook() {
+        boolean check = true;
         System.out.println("Введите название книга, которого вы хотите удалить: ");
         String titleBookDelete=in.nextLine();
         for(int i = 0 ; i < AddABook.booksArray.size() ; i++){
             if(titleBookDelete.equals(AddABook.booksArray.get(i).title)){
                 AddABook.booksArray.remove(i);
+                System.out.println("Удалено успешно");
+                check=false;
             }
         }
+        if(check){
+            System.out.println("Этот книги нет в библиотеке");
+        }
+
 
 
 
